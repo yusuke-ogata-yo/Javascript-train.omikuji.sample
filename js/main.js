da2adc7eeefcb53d0442ce1fc116ac9cb6ec146c
緒方祐介 <yusuke-ogata@Yo-MBA.local>
2020-02-11 14:54:42 +0900

    [update] リファクタリング
    配列をよびだす変数を利用しないように変更

--- js/main.js
+++ js/main.js
@@ -5,19 +5,6 @@
   const result = ['大吉', '中吉', '凶', '末吉'];
 
   btn.addEventListener('click', ()=> {
-    const n = Math.floor(Math.random() * result.length);
-    btn.textContent = result[n];
-
-    // switch (n) {
-    //   case 0:
-    //     btn.textContent = '大吉';
-    //     break;
-    //   case 1:
-    //     btn.textContent = '吉';
-    //     break;
-    //   case 2:
-    //     btn.textContent = '凶';
-    //     break;
-    // }
+    btn.textContent = result[Math.floor(Math.random() * result.length)];
   });
 }
