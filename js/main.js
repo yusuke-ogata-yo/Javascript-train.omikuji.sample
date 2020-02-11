2f25ac264844382a35029b8ac218d7ab969492d1
緒方祐介 <yusuke-ogata@Yo-MBA.local>
2020-02-11 14:48:09 +0900

    [update] lesson#08 配列を用いてswich文を削除
    配列を用いることで、swicth文を削除

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@
 
 {
   const btn = document.getElementById('btn');
+  const result = ['大吉', '中吉', '凶'];
 
   btn.addEventListener('click', ()=> {
     const n = Math.floor(Math.random() * 3);
-    btn.textContent = n;
+    btn.textContent = result[n];
 
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+    // }
   });
 }
